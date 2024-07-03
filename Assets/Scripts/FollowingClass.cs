7451abdf62319b66d9eb3afc5cc05888da443714
IncludeLuisFerreira <prgluisfelipe@gmail.com>
2024-07-03 10:15:38 -0300

    Adding new class [FollowingClass]- enemys

---
+++ Assets/Scripts/FollowingClass.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingClass : MonoBehaviour
+{
+    private Transform player;
+    private Animator enemyAnim;
+    private float speed;
+    private float maxDistance;
+    private float minDistance;
+
+    public FollowingClass(Transform player, Animator enemyAnim, float speed, float maxDistance, float minDistance)
+    {
+        this.player = player;
+        this.enemyAnim = enemyAnim;
+        this.speed = speed;
+        this.maxDistance = maxDistance;
+        this.minDistance = minDistance;
+    }
+
+    public void Follow(Transform enemy)
+    {
+        float distanceToPlayer = Vector3.Distance(enemy.position, player.position);
+
+        if (distanceToPlayer < minDistance && distanceToPlayer < maxDistance)
+        {
+            Vector3 direction = (player.position - enemy.position).normalized;
+            enemy.position += speed * Time.deltaTime * direction;
+            enemyAnim.SetBool("Run", true);
+        }
+        else
+            enemyAnim.SetBool("Run", false);
+    }
+}
--- Assets/Scripts/Golem_Script.cs
+++ Assets/Scripts/Golem_Script.cs
@@ -2,16 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class Golem_Script : MonoBehaviour
 {
+
+    public enum golemState {Attacking_1, None}
+    golemState state = golemState.None;
     [SerializeField]float vida = 15f;
+    [SerializeField]float speed = 2f;
+    [SerializeField]float decendingSpeed = 1.5f;
     [SerializeField]Transform Target;
     [SerializeField]float MinDistance = 10f;
+    [SerializeField]float MaxDistance = 15f;
     Rigidbody2D rb;
     Animator anim;
     bool facingLeft = true;
+    bool isDecending = false;
+    bool isAcending = false;
+    [SerializeField]float groundYPosition;
+    [SerializeField]float originalYPosition;
+
+    [Header("Campos de duração das habilidades")]
+    [SerializeField]float duration_Earthqueak = 2.0f;
+
+    private FollowingClass follow;
 
     private void Awake()
     {
@@ -20,18 +37,21 @@ public class Golem_Script : MonoBehaviour
     }
     void Start()
     {
+        follow = new(Target, anim, speed, MaxDistance, MinDistance);
     }
-
+ 
     void Update()
     {
         Flip();
-        EnemyAttack();        
-        Follow();
+        EnemyAttack();
+        if (state == golemState.None) {
+            follow.Follow(transform);
+        }
     }
 
     float TargetDistance()
     {
-        return Mathf.Abs(Target.position.x - transform.position.x);
+        return Vector3.Distance(transform.position, Target.position);
     }
 
     void Flip()
@@ -55,19 +75,65 @@ public class Golem_Script : MonoBehaviour
         Attack3();
     }
 
-    void Attack1()
+    void Descending()
     {
-        if (TargetDistance() < 5)
+        float newYPosition = Mathf.MoveTowards(transform.position.y, groundYPosition, decendingSpeed * Time.deltaTime);
+        transform.position = new Vector3(transform.position.x, newYPosition, 0);
+
+        if (transform.position.y - 1.35f <= groundYPosition)
         {
-            anim.SetTrigger("Attack");
+            isDecending = false;
         }
     }
 
-    void Follow()
+    void Acending()
     {
-        if (TargetDistance() < MinDistance)
+        float newYPosition = Mathf.MoveTowards(transform.position.y,originalYPosition, decendingSpeed * Time.deltaTime);
+        transform.position = new Vector3(transform.position.x, newYPosition, transform.position.z);
+
+        if (transform.position.y == originalYPosition)
         {
-            rb.velocity = Vector2.MoveTowards(transform.position, Target.position, MinDistance - 1f);
+            isAcending = false;
+        }
+    }
+
+    void StartDecending()
+    {
+        isDecending = true;
+    }
+    
+
+    void StartAcending()
+    {
+        isAcending = true;
+    }
+
+    void Attack1()
+    {
+        if (TargetDistance() < 3f && !isDecending)
+        {
+            StartDecending(); 
+        }
+
+        if (isDecending)
+        {
+            Descending();
+        }
+        
+        if (!isDecending && transform.position.y -1.35 <= groundYPosition && TargetDistance() <= 3f)
+        {
+            anim.SetTrigger("Attack");
+            Earthqueak();
+        }
+
+        if (transform.position.y - 1.35 <= groundYPosition && TargetDistance() > 3f)
+        {
+           StartAcending();
+        }
+
+        if (isAcending)
+        {
+            Acending();
         }
     }
 
@@ -77,5 +143,18 @@ public class Golem_Script : MonoBehaviour
         {
             anim.SetTrigger("Attack3");
         }
+    }
+
+
+    /************************ Ataque 1************************/
+
+
+    IEnumerable Earthqueak()
+    {
+        state = golemState.Attacking_1;
+        rb.velocity = new Vector2(0f, 0f);
+        yield return  new WaitForSeconds(duration_Earthqueak);
+        state = golemState.None;
+        
     }
 }
